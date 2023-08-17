66c3e7c783ed894fcc054b278453500fa6e49cb7
beomq <beomdolq@gmail.com>
2023-08-17 19:03:16 +0900

    시간이 한자리 일 떄 두자리로 표현하게 변경

--- lib/ui/widget/custom_time_text.dart
+++ lib/ui/widget/custom_time_text.dart
@@ -11,11 +11,11 @@ class CustomTimeText extends StatelessWidget {
   Widget build(BuildContext context) {
     return time.inHours > 0
         ? Text(
-            '${time.inHours} : ${time.inMinutes.remainder(60)} : ${time.inSeconds.remainder(60)} ',
+            '${time.inHours.toString().padLeft(2, '0')} : ${time.inMinutes.remainder(60).toString().padLeft(2, '0')} : ${time.inSeconds.remainder(60).toString().padLeft(2, '0')} ',
             style: TextStyle(fontSize: 40),
           )
         : Text(
-            '${time.inMinutes.remainder(60)} : ${time.inSeconds.remainder(60)} ',
+            '${time.inMinutes.remainder(60).toString().padLeft(2, '0')} : ${time.inSeconds.remainder(60).toString().padLeft(2, '0')} ',
             style: TextStyle(fontSize: 40));
   }
 }
