338bc460a2866a69c5d1b59672a900df713f9b47
beomq <beomdolq@gmail.com>
2023-08-21 00:02:44 +0900

    study time input screen 텍스트 수정

--- lib/ui/input/study_time_input_screen.dart
+++ lib/ui/input/study_time_input_screen.dart
@@ -43,91 +43,117 @@ class _StudyTimeInputScreenState extends State<StudyTimeInputScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Container(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Welcome to BreakSchedule!'),
-            Text('a'),
-            Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  CustomTextFormField(
-                    controller: studyTimeController,
-                    hintText: '11:11:11',
-                    labelText: '총공부시간',
-                    validator: (String? value) {
-                      if (!RegExp(r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$')
-                          .hasMatch(value!)) {
-                        return '00:00:00 ~ 23:59:59 사이의 시간을 입력해주세요';
-                      }
-                      return null;
-                    },
-                  ),
-                  CustomTextFormField(
-                    controller: minBreakTimeController,
-                    hintText: '11:11',
-                    labelText: '최소 쉬는시간',
-                    validator: (String? value) {
-                      if (!RegExp(r'^([0-5]?\d):([0-5]\d)$').hasMatch(value!)) {
-                        return '00:01 ~ 59:59 사이의 시간을 입력해주세요';
-                      }
-                      return null;
-                    },
-                  ),
-                  CustomTextFormField(
-                    controller: maxBreakTimeController,
-                    hintText: '11:11',
-                    labelText: '최대 쉬는시간',
-                    validator: (String? value) {
-                      if (!RegExp(r'^([0-5]?\d):([0-5]\d)$').hasMatch(value!)) {
-                        return '00:01 ~ 59:59 사이의 시간을 입력해주세요';
-                      }
-                      return null;
-                    },
-                  ),
-                ],
+        body: SingleChildScrollView(
+      child: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Welcome to Random Break Timer!',
+                style: TextStyle(fontSize: 24),
               ),
-            ),
-            CustomButton(
-              onPressed: () {
-                final formKeyState = formKey.currentState!;
-                if (formKeyState.validate()) {
-                  formKeyState.save();
-                  final minTime =
-                      timeStringToSeconds(minBreakTimeController.text);
-                  final maxTime =
-                      timeStringToSeconds(maxBreakTimeController.text);
-
-                  if (minTime > maxTime) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('최대 쉬는시간은 항상 최소 쉬는시간 보다 커야 합니다'),
-                        duration: Duration(seconds: 1), // 메시지 표시 시간 설정
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                '얼마나 쉬어야 공부 효율이 좋을까요? \n내가 설정한 범위 내의 랜덤한 시간이 쉬는시간으로 주어집니다.',
+                style: TextStyle(fontSize: 13),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      CustomTextFormField(
+                        controller: studyTimeController,
+                        hintText: 'ex) 12:00:00',
+                        labelText: 'Total Study Time',
+                        validator: (String? value) {
+                          if (!RegExp(r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$')
+                              .hasMatch(value!)) {
+                            return '00:00:00 ~ 23:59:59 사이의 시간을 입력해주세요';
+                          }
+                          return null;
+                        },
+                        icon: Icons.timer_sharp,
                       ),
-                    );
-                    return;
-                  }
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => TimerHomePage(
-                              totalStudyTime:
-                                  timeStringToSeconds(studyTimeController.text),
-                              minBreakTime: timeStringToSeconds(
-                                  minBreakTimeController.text),
-                              maxBreakTime: timeStringToSeconds(
-                                  maxBreakTimeController.text),
-                            )),
-                  );
-                }
-              },
-              text: 'Start Study',
-            ),
-          ],
+                      CustomTextFormField(
+                        controller: minBreakTimeController,
+                        hintText: 'ex) 15:00',
+                        labelText: 'Min Break Time',
+                        validator: (String? value) {
+                          if (!RegExp(r'^([0-5]?\d):([0-5]\d)$')
+                              .hasMatch(value!)) {
+                            return '00:01 ~ 59:59 사이의 시간을 입력해주세요';
+                          }
+                          return null;
+                        },
+                        icon: Icons.local_cafe_outlined,
+                      ),
+                      CustomTextFormField(
+                        controller: maxBreakTimeController,
+                        hintText: 'ex) 20:00',
+                        labelText: 'Max Break Time',
+                        validator: (String? value) {
+                          if (!RegExp(r'^([0-5]?\d):([0-5]\d)$')
+                              .hasMatch(value!)) {
+                            return '00:01 ~ 59:59 사이의 시간을 입력해주세요';
+                          }
+                          return null;
+                        },
+                        icon: Icons.local_cafe,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: CustomButton(
+                  onPressed: () {
+                    final formKeyState = formKey.currentState!;
+                    if (formKeyState.validate()) {
+                      formKeyState.save();
+                      final minTime =
+                          timeStringToSeconds(minBreakTimeController.text);
+                      final maxTime =
+                          timeStringToSeconds(maxBreakTimeController.text);
+
+                      if (minTime > maxTime) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('최대 쉬는시간은 항상 최소 쉬는시간 보다 커야 합니다'),
+                            duration: Duration(seconds: 1), // 메시지 표시 시간 설정
+                          ),
+                        );
+                        return;
+                      }
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => TimerHomePage(
+                                  totalStudyTime: timeStringToSeconds(
+                                      studyTimeController.text),
+                                  minBreakTime: timeStringToSeconds(
+                                      minBreakTimeController.text),
+                                  maxBreakTime: timeStringToSeconds(
+                                      maxBreakTimeController.text),
+                                )),
+                      );
+                    }
+                  },
+                  text: 'Start Study',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     ));
