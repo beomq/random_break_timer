caa78223d94bf8282bcbae09e134ac8b13d9b91f
beomq <beomdolq@gmail.com>
2023-08-20 23:59:22 +0900

    custom text form field widget 수정

--- lib/ui/widget/custom_text_form_field.dart
+++ lib/ui/widget/custom_text_form_field.dart
@@ -4,28 +4,47 @@ class CustomTextFormField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final String labelText;
-  final FormFieldValidator<String>? validator; // validator 추가
+  final FormFieldValidator<String>? validator;
+  final IconData icon;
 
   CustomTextFormField({
     required this.controller,
     required this.hintText,
     required this.labelText,
-    required this.validator, // validator 초기화
+    required this.validator,
+    required this.icon, // validator 초기화
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      onSaved: (value) => controller.text = value!,
-      controller: controller,
-      decoration: InputDecoration(
-        hintText: hintText,
-        labelStyle: const TextStyle(
-          fontSize: 11,
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 40,
         ),
-        labelText: labelText,
-      ),
-      validator: validator,
+        const SizedBox(
+          width: 20,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(labelText),
+              TextFormField(
+                onSaved: (value) => controller.text = value!,
+                controller: controller,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: hintText,
+                ),
+                validator: validator,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
