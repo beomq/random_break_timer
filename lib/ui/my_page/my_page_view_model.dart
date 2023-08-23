2540a4ee6d6f84865fb6eb14785e10b03959f249
beomq <beomdolq@gmail.com>
2023-08-23 09:38:42 +0900

    전체삭제 선택삭제 기능 추가

--- lib/ui/my_page/my_page_view_model.dart
+++ lib/ui/my_page/my_page_view_model.dart
@@ -1,8 +1,28 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:random_break_timer/data/model/study_data.dart';
+import 'package:random_break_timer/main.dart';
 
 class MyPageViewModel {
+  Future<List<StudyData>> getCachedStudyData() async {
+    var data = await Hive.box<StudyData>(getUserUid());
+    return data.values.toList();
+  }
+
   void logout() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  void allDelete() async {
+    var box = await Hive.box<StudyData>(getUserUid());
+    await box.clear();
+  }
+
+  void deleteItemAtIndex(int index) async {
+    var box = await Hive.box<StudyData>(getUserUid());
+    if (index < box.length) {
+      await box.deleteAt(index);
+    }
   }
 
   String getNickname() {
